23cc68adcfaca31f48483d511b62434ca7eaa9a1
metaStor <metashen@qq.com>
2020-09-07 11:35:26 +0800

    delete Referer of headers

--- urlChecker.py
+++ urlChecker.py
@@ -13,8 +13,7 @@ headers = {
     'Accept-Language': 'en-US,en;q=0.8',
     'Cache-Control': 'max-age=0',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
-    'Connection': 'keep-alive',
-    'Referer': ''
+    'Connection': 'keep-alive'
 }
 
 
