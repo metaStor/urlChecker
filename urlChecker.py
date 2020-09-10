f00d1518488148147c59ba1be6e874e70c4a86f9
metaStor <metashen@qq.com>
2020-09-10 20:50:25 +0800

    add title with exist url

--- urlChecker.py
+++ urlChecker.py
@@ -1,12 +1,11 @@
 import requests
 import sys
 import ssl
+import re
 import getopt
-
 
 requests.packages.urllib3.disable_warnings()
 ssl._create_default_https_context = ssl._create_unverified_context
-
 
 headers = {
     'Accept': '*/*',
@@ -20,18 +19,22 @@ headers = {
 def get_status(url):
     try:
         r = requests.get(url, allow_redirects=True, verify=False, timeout=5, headers=headers)
-        if r.status_code >= 200 and r.status_code <= 206:
-            print("[*] \033[1;32;44m%s\033[0m Exist!" % url)
-            return True
+        if 200 <= r.status_code <= 206:
+            # 获取title
+            r.encoding = 'UTF-8'
+            content = r.text
+            title = re.findall('<title>(.+)</title>', content)
+            print("\033[1;32;40m[*] %s Exist! >> %s\033[0m" % (url, title))
+            return True, title[0]
         else:
             print("[!] %s Not Exist!" % url)
-            return False
+            return False, None
     except:
         print("[!] %s Not Exist!" % url)
-        return False
-    
-def main(argv):
+        return False, None
 
+
+def main(argv):
     file, out = '', ''
     res = []
 
@@ -52,19 +55,22 @@ def main(argv):
     if file == '' or out == '':
         print('checkUrl.py -f <file> -o <out>')
         sys.exit(2)
-        
+
     with open(file, 'r') as fp:
         url = fp.readline().strip()
         while url:
             status = get_status(url)
-            if status:
-                res.append(url)
+            if status[0]:
+                # 当前站点信息：url, title
+                info = [url, status[1]]
+                res.append(info)
             url = fp.readline().strip()
     if len(res) > 0:
         with open(out, 'w') as fp:
             for u in res:
-                fp.write(u + '\n')
-        print("[*] \033[1;33;44mSave result in %s !\033[0m" % out)
+                fp.write(u[0] + '\t>>\t' + u[1] + '\n')
+        print("\033[1;32;40m[*] Save result in %s !\033[0m" % out)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main(sys.argv[1:])
